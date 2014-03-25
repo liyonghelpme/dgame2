8096dbc5c536348cc65079396dafd22aec1872c4
liyonghelpme <liyonghelpme@gmail.com>
2014-03-25 16:28:18 +0800

    player health big

---
+++ Assets/ZombieNet.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieNet : Photon.MonoBehaviour {
+	void Awake() {
+		if(photonView.isMine){
+		}else {
+		}
+		gameObject.name = gameObject.name+photonView.viewID;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		//write my data to server
+		if(stream.isWriting){
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		}else {
+			correctPos = (Vector3)stream.ReceiveNext();
+			correctRot = (Quaternion)stream.ReceiveNext();
+		}
+	}
+	//only master server zombie can attack 
+	private Vector3 correctPos = Vector3.zero;
+	private Quaternion correctRot = Quaternion.identity;
+	
+	// Update is called once per frame
+	void Update () {
+		if(!photonView.isMine) {
+			transform.position = Vector3.Lerp(transform.position, correctPos, Time.deltaTime*5);
+			transform.rotation = Quaternion.Slerp(transform.rotation, correctRot, Time.deltaTime*5);
+		}
+	}
+	//level 
+	void OnPhotonInstantiate(PhotonMessageInfo info) {
+		object[] objs = photonView.instantiationData;
+		int lv = (int)(objs[0]);
+		GetComponent<MyStatus>().SetLevel(lv);
+	}
+}
+
